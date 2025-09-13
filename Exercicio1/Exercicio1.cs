e478737d6688db934863dda1009ffc392cefc7c2
Gilmar Junior <rochagmjunior@gmail.com>
2025-09-13 17:21:24 -0300

    Atualização do código

---
+++ Exercicio1/Exercicio1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+Console.WriteLine("--SOMA E MÉDIA--\n");
+
+List<float> numeros = [];
+float numero;
+
+Menu();
+void Menu()
+{
+    Console.Write("Quantos números deseja usar? (3-10): ");
+    int qtNumeros = int.Parse(Console.ReadLine()!);
+    Console.Write("\n");
+    if (qtNumeros < 3 | qtNumeros > 10)
+    {
+        Console.WriteLine("<ERRO>Quantidade inválida.");
+        return;
+    }
+
+    for (int i = 1; i <= qtNumeros; i++)
+    {
+        Console.Write($"{i}º número: ");
+        string? input = Console.ReadLine();
+        if (float.TryParse(input, out numero))
+            numeros.Add(numero);
+        else
+        {
+            Console.WriteLine("<ERRO>Valor inválido, tente novamente.");
+            i--;
+        }
+    }
+
+    foreach (float num in numeros)
+    {
+        float aux = num;
+       // TODO: Como usar foreach para somar os valores dentro da List "numeros"? 
+    }
+}
--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # Exercicios em C#
 
 Exercícios para treinar (lógica/sintaxe).
-
--> Usando Visual Studio 2022.
--> PC novo :D (agora dá pra brincar!).
